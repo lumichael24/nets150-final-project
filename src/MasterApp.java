2bd7aff2af47ecfac6bda97388bbb7d7eee61187
lumichael24 <gongminglu@gmail.com>
2020-04-29 21:16:10 -0500

    hardcoded replacement of query text -- need to change to be dynamic with query input

--- src/MasterApp.java
+++ src/MasterApp.java
@@ -4,7 +4,7 @@ public class MasterApp {
 		port(8080);
 		get("/", (request, response) -> {
 			return "<div align=\"center\"> <form action=\"/render\" method=\"GET\">" + 
-					"  <label for=\"fname\">URL Of Website:</label>" + 
+					"  <label for=\"fname\">Type the URL of the Website You Want to Visit:</label>" + 
 					"  <input style=\"height:50px;font-size:12pt;width:400px;\" type=\"text\" id=\"url\" name=\"url\" placeholder=\"e.g. https://www.espn.com\"><br><br><br>" + 
 					"  <input type=\"submit\" value=\"Submit\" style=\"height:100px; width:100px\">" +
 					"</form></div>";
@@ -19,6 +19,7 @@ public class MasterApp {
 			try {
 				Client c = new Client().connect(url);
 				String contents = c.readContents();
+				contents = contents.replaceAll("Favre", "<strong> Favre</strong>");
 				return contents;
 			} catch (Exception e) {
 				e.printStackTrace();
