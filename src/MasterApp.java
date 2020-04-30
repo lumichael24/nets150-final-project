e3041b942fd04a63f7bc0dfab494a780c31c2dd5
lumichael24 <gongminglu@gmail.com>
2020-04-29 20:37:00 -0500

    finishedsearch bar + render

--- src/MasterApp.java
+++ src/MasterApp.java
@@ -3,7 +3,22 @@ public class MasterApp {
 	public static void main(String[] args) {
 		port(8080);
 		get("/", (request, response) -> {
-			return null;
+			return "<form action=\"/render\" method=\"GET\">" + 
+					"  <label for=\"fname\">URL Of Website:</label><br>" + 
+					"  <input type=\"text\" id=\"url\" name=\"url\"><br>" + 
+					"  <input type=\"submit\" value=\"Submit\">" +
+					"</form>";
+		});
+		
+		get("/render", (request, response) -> {
+			String url = request.queryParams("url");
+			if (url == null || url == "") {
+				response.redirect("/");
+				return null;
+			}
+			Client c = new Client().connect(url);
+			String contents = c.readContents();
+			return contents;
 		});
 	}
 }
